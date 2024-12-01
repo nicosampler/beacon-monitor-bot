7e0c33a5c8b0eb1b31ba966ae11119e3aa21dbae
nicosampler <nf.dominguez.87@gmail.com>
2024-12-01 17:13:53 -0300

    re-enable bot

--- packages/telegram-bot/src/scheduler/tasks/updateUsersStats.ts
+++ packages/telegram-bot/src/scheduler/tasks/updateUsersStats.ts
@@ -17,10 +17,7 @@ import createLogger from "@/src/lib/pino.js";
 export async function updateUsersStatsImp(userId?: number) {
   const users = await getAllUserIds_db(userId);
 
-  const userChunks = chunk(
-    users.filter((user) => user.username == "nfd_87"),
-    5
-  );
+  const userChunks = chunk(users, 5);
 
   for (const currentChunk of userChunks) {
     await Promise.all(
